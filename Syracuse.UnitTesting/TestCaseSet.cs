9aa53da0cff7074752c7b2759b77eb008532809d
Friedrich von Never <friedrich@fornever.me>
2017-05-06 12:47:55 +0700

    syracuse-tools: move the project to the common root

---
+++ Syracuse.UnitTesting/TestCaseSet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Syracuse.Common;
+
+namespace Syracuse.UnitTesting
+{
+    public class TestCaseSet<TCase> : KeyedCollection<string, TestCaseGroup<TCase>>
+        where TCase : new()
+    {
+        public static TestCaseSet<TCase> FromFile(string path)
+        {
+            using (var reader = new TestCaseReader<TCase>(path))
+            {
+                var set = new TestCaseSet<TCase>();
+                set.AddRange(reader.ReadAllTestCaseGroups());
+                return set;
+            }
+        }
+
+        public static TestCaseSet<TCase> FromStream(Stream stream)
+        {
+            using (var reader = new TestCaseReader<TCase>(stream))
+            {
+                var set = new TestCaseSet<TCase>();
+                set.AddRange(reader.ReadAllTestCaseGroups());
+                return set;
+            }
+        }
+
+        public TestCaseSet()
+            : base()
+        {
+        }
+
+        protected override string GetKeyForItem(TestCaseGroup<TCase> item)
+        {
+            return item.Name;
+        }
+    }
+}
---
+++ Syracuse.UnitTesting/TreeTextRenderer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TexDotNet;
+
+namespace Syracuse.UnitTesting
+{
+    public sealed class TreeTextRenderer
+    {
+        public static string GetText(TexExpressionNode tree)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                var treeTextRenderer = new TreeTextRenderer(stringWriter);
+                treeTextRenderer.Render(tree);
+                return stringWriter.ToString();
+            }
+        }
+
+        public TreeTextRenderer(TextWriter writer)
+        {
+            this.Writer = writer;
+        }
+
+        public TextWriter Writer
+        {
+            get;
+            private set;
+        }
+
+        public void Render(TexExpressionNode tree)
+        {
+            Render(tree, false, 0);
+        }
+
+        private void Render(TexExpressionNode node, bool isArgument, int level)
+        {
+            // Prefix argument nodes with '@'.
+            var indentation = new string(' ', level * 2);
+            this.Writer.WriteLine(indentation + (isArgument ? "@" : string.Empty) + node.ToString());
+            if (node.Children != null)
+            {
+                if (node.Arguments != null)
+                {
+                    foreach (var childNode in node.Arguments)
+                        Render(childNode, true, level + 1);
+                }
+                foreach (var childNode in node.Children)
+                    Render(childNode, false, level + 1);
+            }
+        }
+    }
+}
